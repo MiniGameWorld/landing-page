cf3b9d951b426d675f42a62cdf33c0eb3b049c07
balayokesh <balayokeshmani@gmail.com>
2022-01-22 00:37:08 +0530

    Update CRA wih PTG details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to <code>Part Time Gamer's</code> portfolio.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://linktr.ee/parttimegamer"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get our social media links
         </a>
       </header>
     </div>
